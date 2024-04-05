54001380552266cfc300292e83f699572467574e
kaien07 <160471571+kaien07@users.noreply.github.com>
2024-04-05 14:54:50 +0000

    Final submission

--- main.py
+++ main.py
@@ -1,2 +1,39 @@
 # Built-in imports
+def reverse(text):
+    """
+    Takes in a string and returns the reverse of the string
 
+    Parameters
+    -----------
+    text: string
+
+    Returns
+    -----------
+    string
+    """
+    if len(text) == 0:
+        return ""
+    else:
+        return reverse(text[1:]) + text[0]
+
+
+def is_palindrome(text):
+    """
+    Takes in a string and checks if it is a palindrome
+
+    Parameters
+    ------------
+    text: string
+
+    Returns
+    ------------
+    boolean
+    """
+    text = text.lower().replace(' ', '').replace(',', '')
+    if len(text) == 0:
+        return True
+    else:
+        if text[0] == text[-1]:
+            return is_palindrome(text[1:-1])
+        else:
+            return False
